fddbb42351746d93030233d38dcc4fb26955ad26
NIRMAL <nirmalnirmalkhipro@gmail.com>
2024-06-24 09:57:46 +0500

    added html css and js files

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let input = document.getElementById('Input');
+let buttons = document.querySelectorAll('button');
+let string = "";
+let ary = Array.from(buttons);
+
+ary.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        } else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        } else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string; // Update input value
+        } else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
